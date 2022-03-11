2506f5e55d110db9029c675eb41cc9343585c707
Richard Hallett <richard@richardhallett.com>
2022-03-11 15:46:25 +0100

    Remove trailing slash by default

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -32,7 +32,7 @@ func main() {
 	// Get configuration from environment variables.
 	config := Config{}
 	config.HTTP.Addr = getEnv("HTTP_ADDR", ":8081")
-	config.Plausible.Url = getEnv("PLAUSIBLE_URL", "https://analytics.stage.datacite.org/")
+	config.Plausible.Url = getEnv("PLAUSIBLE_URL", "https://analytics.stage.datacite.org")
 	config.DataCite.Url = getEnv("DATACITE_API_URL", "https://api.stage.datacite.org")
 
 	// Run with configuration.
