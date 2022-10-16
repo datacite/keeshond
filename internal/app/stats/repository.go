d355592f25307f2c1af14de915fa7cacf210fb22
Richard Hallett <rhallett@datacite.org>
2022-10-16 21:58:19 +0200

    Add more stats queries
    Includes helpers for intervals
    Tidies up reuse
    Adds in the counter compliace dedupe logic.

--- internal/app/stats/repository.go
+++ internal/app/stats/repository.go
@@ -7,9 +7,17 @@ import (
 )
 
 type StatsRepositoryReader interface {
+	// Total event metric counts grouped for a specific PID
+	GetTotalForInterval(metricName string, repoId string, pid string, interval func(*gorm.DB) *gorm.DB) int64
 	GetTotalInToday(metricName string, repo_id string, pid string) int64
 	GetTotalInLast7Days(metricName string, repo_id string, pid string) int64
 	GetTotalInLast30Days(metricName string, repo_id string, pid string) int64
+
+	// Total event metric counts grouped by PID
+	GetTotalsByPidForInterval(metricName string, repo_id string, interval func(*gorm.DB) *gorm.DB) []struct{Pid string; Count int64}
+	GetTotalsByPidInToday(metricName string, repo_id string) []struct{Pid string; Count int64}
+	GetTotalsByPidInLast7Days(metricName string, repo_id string) []struct{Pid string; Count int64}
+	GetTotalsByPidInLast30Days(metricName string, repo_id string) []struct{Pid string; Count int64}
 	// GetUniqueInToday(metricName string, repo_id string, pid string) int64
 	// GetUniqueInLast7Days(metricName string, repo_id string, pid string) int64
 	// GetUniqueInLast30Days(metricName string, repo_id string, pid string) int64
@@ -19,54 +27,87 @@ type StatsRepository struct {
 	db 		*gorm.DB
 }
 
-func NewRepository(db *gorm.DB) *StatsRepository {
+func NewStatsRepository(db *gorm.DB) *StatsRepository {
 	return &StatsRepository{
 		db: db,
 	}
 }
 
-func (repository *StatsRepository) GetTotalInToday(metricName string, repoId string, pid string) int64 {
+// This query is for getting based on an interval the total unique metric events
+// We do an inner CTE to be able to remove duplicates based on the interval of 30 secs
+// We then can count the returning CTE table to get our total de duplicated events.
+// For COUNTER this is referred to as Double Click filtering.
+func (repository *StatsRepository) GetTotalForInterval(metricName string, repoId string, pid string, interval func(*gorm.DB) *gorm.DB) int64 {
 	var count int64
 	repository.db.
 	Clauses(
 		exclause.NewWith(
 			"interval_calc", repository.db.Model(&event.Event{}).
 			Select("toStartOfInterval(timestamp, INTERVAL 30 second) as interval_alias").
-			Scopes(NameAndRepoId(metricName, repoId), PID(pid), TimestampIsToday).
+			Scopes(NameAndRepoId(metricName, repoId), PID(pid), interval).
 			Group("interval_alias"),
 		),
 	).Table("interval_calc").Count(&count)
 	return count
 }
 
-func (repository *StatsRepository) GetTotalInLast7Days(metricName string, repoId string, pid string) int64 {
-	var count int64
+func (repository *StatsRepository) GetTotalInToday(metricName string, repo_id string, pid string) int64 {
+	return repository.GetTotalForInterval(metricName, repo_id, pid, TimestampIsToday)
+}
+
+func (repository *StatsRepository) GetTotalInLast7Days(metricName string, repo_id string, pid string) int64 {
+	return repository.GetTotalForInterval(metricName, repo_id, pid, TimestampIn7Days)
+}
+
+func (repository *StatsRepository) GetTotalInLast30Days(metricName string, repo_id string, pid string) int64 {
+	return repository.GetTotalForInterval(metricName, repo_id, pid, TimestampIn30Days)
+}
+
+// Similiar to the query for specific pid, builds a CTE that is grouped by the interval of 30 secs to remove duplicates in interval
+// But also returns the pid and then we do a group by again to get counts for pids.
+func (repository *StatsRepository) GetTotalsByPidForInterval(metricName string, repoId string, interval func(*gorm.DB) *gorm.DB) []struct{Pid string; Count int64} {
+	var result []struct {
+		Pid string
+		Count int64
+	}
 	repository.db.
 	Clauses(
 		exclause.NewWith(
 			"interval_calc", repository.db.Model(&event.Event{}).
-			Select("toStartOfInterval(timestamp, INTERVAL 30 second) as interval_alias").
-			Scopes(NameAndRepoId(metricName, repoId), PID(pid), TimestampIn7Days).
-			Group("interval_alias"),
+			Select("pid, toStartOfInterval(timestamp, INTERVAL 30 second) as interval_alias").
+			Scopes(NameAndRepoId(metricName, repoId), interval).
+			Group("pid, interval_alias"),
 		),
-	).Table("interval_calc").Count(&count)
-	return count
+	).Table("interval_calc").Select("pid, count(*) as count").Group("pid").Limit(10).Scan(&result)
+	return result
 }
 
-func (repository *StatsRepository) GetTotalInLast30Days(metricName string, repoId string, pid string) int64 {
-	var count int64
-	repository.db.
-	Clauses(
-		exclause.NewWith(
-			"interval_calc", repository.db.Model(&event.Event{}).
-			Select("toStartOfInterval(timestamp, INTERVAL 30 second) as interval_alias").
-			Scopes(NameAndRepoId(metricName, repoId), PID(pid), TimestampIn30Days).
-			Group("interval_alias"),
-		),
-	).Table("interval_calc").Count(&count)
-	return count
+func (repository *StatsRepository) GetTotalsByPidInToday(metricName string, repo_id string) []struct{Pid string; Count int64} {
+	return repository.GetTotalsByPidForInterval(metricName, repo_id, TimestampIsToday)
 }
 
+func (repository *StatsRepository) GetTotalsByPidInLast7Days(metricName string, repo_id string) []struct{Pid string; Count int64} {
+	return repository.GetTotalsByPidForInterval(metricName, repo_id, TimestampIn7Days)
+}
+
+func (repository *StatsRepository) GetTotalsByPidInLast30Days(metricName string, repo_id string) []struct{Pid string; Count int64} {
+	return repository.GetTotalsByPidForInterval(metricName, repo_id, TimestampIn30Days)
+}
+
+// Following are scopes for the event model that can be used
+// to build up queries dynamically.
+
+// func IntervalCalcCTE(scopes ...func(*gorm.DB) *gorm.DB) func(*gorm.DB) *gorm.DB {
+// 	return func(db *gorm.DB) *gorm.DB {
+// 		return db.Clauses(exclause.NewWith("interval_calc", db.Model(&event.Event{}).Scopes(Interval30Sec + scopes...)))
+// 	}
+// }
+
+// func Interval30Sec(db *gorm.DB) *gorm.DB {
+// 	return db.
+// 	Select("toStartOfInterval(timestamp, INTERVAL 30 second) as interval_alias").
+// 	Group("interval_alias")
+// }
 
 func NameAndRepoId(name string, repo_id string) func (db *gorm.DB) *gorm.DB {
 	return func (db *gorm.DB) *gorm.DB {
--- internal/app/stats/service.go
+++ internal/app/stats/service.go
@@ -22,3 +22,15 @@ func (service *StatsService) GetTotalInLast7Days(metricName string, repoid strin
 func (service *StatsService) GetTotalInLast30Days(metricName string, repoid string, pid string) int64 {
 	return service.repository.GetTotalInLast30Days(metricName, repoid, pid)
 }
+
+func (service *StatsService) GetTotalsByPidInToday(metricName string, repoid string) []struct{Pid string; Count int64} {
+	return service.repository.GetTotalsByPidInToday(metricName, repoid)
+}
+
+func (service *StatsService) GetTotalsByPidInLast7Days(metricName string, repoid string) []struct{Pid string; Count int64} {
+	return service.repository.GetTotalsByPidInLast7Days(metricName, repoid)
+}
+
+func (service *StatsService) GetTotalsByPidInLast30Days(metricName string, repoid string) []struct{Pid string; Count int64} {
+	return service.repository.GetTotalsByPidInLast30Days(metricName, repoid)
+}
